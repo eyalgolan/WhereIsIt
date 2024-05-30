16f578f33fec3a8f685c8693c221c7071fec2c22
Eyal <eyal@papercup.com>
2024-05-30 14:39:50 +0100

    feat: wip add a script that will scrape the lines locations from the tfl api

--- whereisit/scripts/locations_scraper.py
+++ whereisit/scripts/locations_scraper.py
@@ -1,8 +1,8 @@
 # This file should hold the logic for getting al the location and station info,
 # in order to locate the trains on the map.
 
-from decimal import Decimal
-from typing import List
+import json
+from typing import Dict, List
 
 import requests
 from pydantic import BaseModel
@@ -11,13 +11,21 @@ from whereisit.constants import TubeLine
 
 
 class Location(BaseModel):
-    lon: Decimal
-    lat: Decimal
+    lon: float
+    lat: float
 
 
 class Station(Location):
-    natpan_id: int
+    natpan_id: str
     name: str
+
+    def to_dict(self) -> Dict:
+        return {
+            "name": self.name,
+            "natpan_id": self.natpan_id,
+            "lon": self.lon,
+            "lat": self.lat,
+        }
 
 
 class Line(BaseModel):
@@ -25,5 +33,29 @@ class Line(BaseModel):
 
 
 for line in TubeLine:
-    url = f"https://api.tfl.gov.uk/line/{line.name}/route/sequence/all"
+    line_stations = []
+    url = f"https://api.tfl.gov.uk/line/{line.value}/route/sequence/all"
     response = requests.request("GET", url).json()
+
+    sub_lines = response["lineStrings"]
+
+    # TODO create a object that describes the order of locations in each subline.
+    # The object should also detail for each location if it's a station.
+
+    try:
+        stations = response["stations"]
+    except KeyError:
+        print(f"API call for line {line.value} didn't return any stations")
+        continue
+    for station in stations:
+        s = Station(
+            natpan_id=station.get("id"),
+            name=station.get("name"),
+            lat=station.get("lat"),
+            lon=station.get("lon"),
+        )
+        line_stations.append(s.to_dict())
+
+    # TODO save jsons in assets folder
+    with open(f"{line.value}_stations.json", "w") as json_file:
+        json.dump(line_stations, json_file, indent=4)
